e50c36c36e66e6cf4257d4ed633cacd36695dcdf
Rajnarayan014 <pulkitsingh7773@gmail.com>
2024-04-26 12:27:27 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,6 +2,17 @@ function longestSubstring(s) {
 
 
     // Implementation of longestSubstring function
+    var n = str.length;
+    
+    // Result
+    var res = 0; 
+    
+    for(var i = 0; i < n; i++)
+        for(var j = i; j < n; j++)
+            if (areDistinct(str, i, j))
+                res = Math.max(res, j - i + 1);
+                
+    return res;
 }
 
 module.exports = { longestSubstring };
