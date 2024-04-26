12832694c23144002afcdedcb4eee85ccda7b0d9
Rajnarayan014 <pulkitsingh7773@gmail.com>
2024-04-26 12:21:52 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,6 +2,43 @@ function smallestMissingPositiveInteger(nums) {
 
 
     // Implement the function smallest_missing_positive_integer
+    let n = A.length;
+        // To mark the occurrence of elements
+        let present = new Array(n+1);
+        
+        
+        for(let i=0;i<n+1;i++)
+        {
+            present[i]=false;
+        }
+        // Mark the occurrences
+        for (let i = 0; i < n; i++)
+        {
+            // Only mark the required elements
+            // All non-positive elements and
+            // the elements greater n + 1 will never
+            // be the answer
+            // For example, the array will be {1, 2, 3}
+            // in the worst case and the result
+            // will be 4 which is n + 1
+            if (A[i] > 0 && A[i] <= n)
+            {
+                present[A[i]] = true;
+            }
+        }
+        // Find the first element which didn't
+        // appear in the original array
+
+        for (let i = 1; i <= n; i++)
+        {
+            if (!present[i])
+            {
+                return i;
+            }
+        }
+        // If the original array was of the
+        // type {1, 2, 3} in its sorted form
+        return n + 1;
   
   }
   
